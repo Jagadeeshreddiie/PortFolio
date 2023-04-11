ab61c5b3b9908db06b7c58ce377cadc855d48cfc
JagadeeshReddy <105237455+Jagadeeshreddiie@users.noreply.github.com>
2023-04-11 20:05:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 AOS.init();
-
 const imgPos=document.getElementById('img-pos');
 const heading=document.getElementById('heading');
 
 window.addEventListener('scroll', () => {
     let { scrollY } = window;
-
-    imgPos.style.top = (0.5) * scrollY + 'px';
+    let value=(0.5) * scrollY 
+    imgPos.style.top = value+ 'px';
     // heading.style.transform = 'translateX('+(0.2*scrollY)+'%)';
     console.log(heading.style.transform);
     
 });
 imgPos.addEventListener('click',()=>{
-    heading.style.transform='translateX(0)';
+    imgPos.style.top=value-10+'px';
 })
 
